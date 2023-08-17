43889cf392cf0c003d2e666c0f347ceee13290ed
TrueShikar <115390854+TrueShikar@users.noreply.github.com>
2023-08-17 17:22:49 +0530

    Update create.php

--- src/create.php
+++ src/create.php
@@ -1,69 +1,61 @@
 <?php
-$id = $_GET['id'];
-$url = 'http://softott.live:80/play/live.php?mac=00:1A:79:00:07:91&stream='.$id.'&extension=m3u8&play_token=shikari';
-$options = array(
-    CURLOPT_URL => $url,
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_HTTPHEADER => array(
-        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.200',
-        'Host: kuchini.site:8080',
-        'Accept: */*'
-    ),
-    CURLOPT_HEADER => true,
-    CURLOPT_FOLLOWLOCATION => false  // Disable automatic redirects
+
+//new curl prep
+$id = $_GET['id']; 
+$ch = curl_init();
+
+$headers = array(
+    "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3",
+    "X-User-Agent: Model: MAG250; Link: WiFi",
+    "Referer: http://watch.push4k.tv/stalker_portal/c/",
+    "Cookie: mac=00:1A:79:58:CC:80; stb_lang=en; timezone=GMT",
+    "Accept: */*",
+    "Host: watch.push4k.tv",
+    "Connection: Keep-Alive"
 );
 
-$ch = curl_init();
-curl_setopt_array($ch, $options);
+$url = "http://watch.push4k.tv/stalker_portal/server/load.php?type=itv&action=create_link&cmd=ffrt%20http%3A%2F%2Flocalhost%2Fch%2F".$id."&JsHttpRequest=1-xml";
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
 $response = curl_exec($ch);
 
-if ($response === false) {
-    $error = curl_error($ch);
-    // Handle the error
+if (curl_errno($ch)) {
+    echo 'Error: ' . curl_error($ch);
 } else {
-    $headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-    $header = substr($response, 0, $headerSize);
-
-    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-    if ($httpCode === 302) {
-        $locationHeader = getHeaderValue($header, 'Location');
-        
-        }
-    }
-
+    echo $response; // Output the response
+}
 
 curl_close($ch);
 
-// Helper function to extract header values by name
-function getHeaderValue($header, $name) {
-    $pattern = '/^' . preg_quote($name) . ': ?(.+?)$/m';
-    if (preg_match($pattern, $header, $matches)) {
-        return $matches[1];
+
+if ($response !== false) {
+    $response_data = json_decode($response, true);
+
+    if ($response_data !== null && isset($response_data['js']['cmd'])) {
+        $cmd = $response_data['js']['cmd'];
+
+        // Convert the "cmd" to its usual form
+        $cmd = str_replace('\/', '/', $cmd);
+        $cmd = str_replace('ffmpeg', '', $cmd);
+       
+    } else {
+        echo "Invalid JSON or missing 'cmd' field in the response.\n";
     }
-    return null;
+} else {
+    echo "Failed to read the response file.\n";
 }
 
-// Parse the URL
-$parsedUrl = parse_url($locationHeader);
 
-// Reconstruct the base URL
-$baseUrl = $parsedUrl['scheme'] . '://' . $parsedUrl['host'] . ':' . $parsedUrl['port'] . '/';
+header("Location: ".$cmd);
 
 
 
 
-$opts = array(
-  'http'=>array(
-    'method'=>"GET",
-  
-  )
-);
-$context = stream_context_create($opts);
-$f = preg_replace("/(?<=ts).*/", "", file_get_contents($locationHeader));
-$g = preg_replace("/.*ts/", "ts.php?ts=".$baseUrl."$0", $f);
 
-echo $g;
 
 
 
