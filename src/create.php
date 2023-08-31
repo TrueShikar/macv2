c070b12c89fe201f5224d4946ce4b3ae3b5b534a
TrueShikar <115390854+TrueShikar@users.noreply.github.com>
2023-08-31 22:51:40 +0530

    Update create.php

--- src/create.php
+++ src/create.php
@@ -1,50 +1,75 @@
 <?php
+$n = $_GET['n'];
+$url = 'http://stb.vooiptv.com:80/play/live.php?mac=00:1A:79:AA:2B:B5&stream='.$n.'&extension=m3u8&play_token=rREBYMtfdv';
 
-//new curl prep
-$id = $_GET['id']; 
-$ch = curl_init();
-
-$headers = array(
-    "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3",
-    "X-User-Agent: Model: MAG250; Link: WiFi",
-    "Referer: http://watch.push4k.tv/stalker_portal/c/",
-    "Cookie: mac=00:1A:79:58:CC:80; stb_lang=en; timezone=GMT",
-    "Accept: */*",
-    "Host: watch.push4k.tv",
-    "Connection: Keep-Alive"
+$options = array(
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => array(
+        'User-Agent: HttpCanary/3.3.5',
+        'Host: sansat.net:25461',
+        'Accept: */*'
+    ),
+    CURLOPT_HEADER => true,
+    CURLOPT_FOLLOWLOCATION => false  // Disable automatic redirects
 );
 
-$url = "http://watch.push4k.tv/stalker_portal/server/load.php?type=itv&action=create_link&cmd=ffrt%20http%3A%2F%2Flocalhost%2Fch%2F".$id."&JsHttpRequest=1-xml";
-
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
+$ch = curl_init();
+curl_setopt_array($ch, $options);
 $response = curl_exec($ch);
 
+if ($response === false) {
+    $error = curl_error($ch);
+    // Handle the error
+} else {
+    $headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+    $header = substr($response, 0, $headerSize);
+
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    if ($httpCode === 302) {
+        $locationHeader = getHeaderValue($header, 'Location');
+        
+        }
+    }
 
 
 curl_close($ch);
 
-
-if ($response !== false) {
-    $response_data = json_decode($response, true);
-
-    if ($response_data !== null && isset($response_data['js']['cmd'])) {
-        $cmd = $response_data['js']['cmd'];
-
-        // Convert the "cmd" to its usual form
-        $cmd = str_replace('\/', '/', $cmd);
-        $cmd = str_replace('ffmpeg', '', $cmd);
-       
-    } else {
-        echo "Invalid JSON or missing 'cmd' field in the response.\n";
+// Helper function to extract header values by name
+function getHeaderValue($header, $name) {
+    $pattern = '/^' . preg_quote($name) . ': ?(.+?)$/m';
+    if (preg_match($pattern, $header, $matches)) {
+        return $matches[1];
     }
-} else {
-    echo "Failed to read the response file.\n";
+    return null;
 }
 
-header("Location: http://watch-it.byethost14.com/mac/plyr.php?id=".$id);
+// Parse the URL
+$parsed_url = parse_url($locationHeader);
+
+// Reconstruct the base URL
+$elink = $parsed_url['scheme'] . "://" . $parsed_url['host'] . ":" . $parsed_url['port'];
+
+
+
+
+$opts = array(
+  'http'=>array(
+    'method'=>"GET",
+  
+  )
+);
+$context = stream_context_create($opts);
+$f = preg_replace("/(?<=ts).*/", "", file_get_contents($locationHeader));
+$g = preg_replace("/.*ts/", "ts.php?ts=".$elink."$0", $f);
+
+
+
+echo $g;
+
+
+
+
 
 ?>
