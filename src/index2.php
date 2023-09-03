2f6db1b457fdbfde80163fe6ece6a47d93e35198
TrueShikar <115390854+TrueShikar@users.noreply.github.com>
2023-09-03 18:49:54 +0530

    Update index2.php

--- src/index2.php
+++ src/index2.php
@@ -69,76 +69,80 @@ $filteredChannels = array_filter($channels, function ($channel) use ($searchQuer
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Channel List</title>
-    <link rel="stylesheet" href="style.css">
     <style>
         body {
-            background-color: #008080; /* Windows desktop background color */
+            background-color: #8c72bd;
             font-family: Arial, sans-serif;
             margin: 0;
             padding: 0;
         }
 
         .container {
-            background-color: #000;
-            color: #fff;
+            background-color: #8c72bd;
+            color: #000;
             padding: 20px;
             font-size: 18px;
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
         }
 
         h1 {
             font-size: 24px;
             margin: 0;
             padding-bottom: 20px;
-            border-bottom: 1px solid #fff;
+            border-bottom: 1px solid #000;
         }
 
         .search-form {
             margin-bottom: 20px;
+            display: flex; /* Display form elements side by side */
+            flex-wrap: wrap; /* Allow wrapping on smaller screens */
         }
 
         .search-form input[type="text"] {
-            padding: 10px;
-            font-size: 18px;
-            width: 70%;
+            padding: 5px; /* Decrease padding */
+            font-size: 16px; /* Decrease font size */
+            width: calc(70% - 10px); /* Adjust width to allow space for the button */
             border: none;
             border-radius: 5px;
         }
 
         .search-form button {
-            padding: 10px 20px;
-            font-size: 18px;
+            padding: 5px 10px; /* Decrease padding */
+            font-size: 16px; /* Decrease font size */
             background-color: #009999;
             color: #fff;
             border: none;
             border-radius: 5px;
             cursor: pointer;
+            margin-left: 10px; /* Add space between input and button */
         }
 
         .channel-list {
             list-style: none;
             padding: 0;
-            display: flex;
-            flex-wrap: wrap;
-            gap: 20px;
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+            gap: 10px;
+            justify-items: center;
         }
 
         .channel-item {
             background-color: #fff;
-            padding: 20px;
-            border-radius: 10px;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            padding: 10px;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
             text-align: center;
-            width: 200px;
         }
 
         .channel-item img {
-            max-width: 100px;
-            max-height: 100px;
+            width: 80px;
+            height: 80px;
+            object-fit: cover;
+            border-radius: 50%;
         }
 
         .channel-item a {
@@ -147,10 +151,22 @@ $filteredChannels = array_filter($channels, function ($channel) use ($searchQuer
             color: #009999;
             font-weight: bold;
             text-decoration: none;
+            font-size: 16px;
+        }
+
+        @media screen and (max-width: 480px) {
+            .search-form input[type="text"] {
+                width: calc(100% - 10px); /* Full width on smaller screens */
+                margin-bottom: 10px; /* Add space between input and button */
+            }
+
+            .search-form button {
+                width: 100%; /* Full width on smaller screens */
+                margin-left: 0; /* Remove margin on smaller screens */
+            }
         }
     </style>
 </head>
-
 <body>
     <div class="container">
         <h1>2000+ Channels</h1>
@@ -182,7 +198,5 @@ $filteredChannels = array_filter($channels, function ($channel) use ($searchQuer
             </ul>
         <?php endif; ?>
     </div>
-
 </body>
-
 </html>
