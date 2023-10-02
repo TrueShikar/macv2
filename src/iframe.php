401481d5ca2267dcf256ab09629d73d02520076c
TrueShikar <115390854+TrueShikar@users.noreply.github.com>
2023-10-02 22:52:42 +0530

    Update iframe.php

--- src/iframe.php
+++ src/iframe.php
@@ -121,17 +121,12 @@ html {
 </style>
 
 </head>
-<body bgcolor = "#c41d66">
-<h4><p> Refresh if don't work.</p>
-<p>Script for sell <a href = "https://t.me/ipl_stream_support_bot">Contact Me</a></p>
-</h4>
 
-
+<body>
 
 <video hideControls="false" autoplay controls crossorigin poster="mp.php?<?php echo $_REQUEST["p"]; ?>" playsinline>
 
     <source type="application/x-mpegURL" src="create.php?id=<?php echo $_REQUEST["id"]; ?>"></video>
-<ins data-ad-format="auto" class="adsbygoogle adsbygoogle-noablate" data-ad-client="ca-pub-6949365608691435" data-adsbygoogle-status="done" style="display: block; margin: auto; background-color: transparent; height: 300px;" data-ad-status="filled"><div id="aswift_3_host" tabindex="0" title="Advertisement" aria-label="Advertisement" style="border: none; height: 300px; width: 360px; margin: 0px; padding: 0px; position: relative; visibility: visible; background-color: transparent; display: inline-block; overflow: visible;"><iframe id="aswift_1" name="aswift_1" style="left:0;position:absolute;top:0;border:0;width:360px;height:300px;" sandbox="allow-forms allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts allow-top-navigation-by-user-activation" width="360" height="300" frameborder="0" marginwidth="0" marginheight="0" vspace="0" hspace="0" allowtransparency="true" scrolling="no" src="https://googleads.g.doubleclick.net/pagead/ads?client=ca-pub-6949365608691435&amp;output=html&amp;h=300&amp;slotname=9093952817&amp;adk=1241010510&amp;adf=1294301629&amp;pi=t.ma~as.9093952817&amp;w=360&amp;rafmt=11&amp;format=360x300&amp;url=https://yo.fan/shikari&amp;host=ca-host-pub-8726151893822553&amp;fwrattr=true&amp;wgl=1&amp;fwr=1&amp;fwrattr=true&amp;rpe=1&amp;resp_fmts=3&amp;sfro=1&amp;wgl=1&amp;dt=1691179907755&amp;bpp=10&amp;bdt=305&amp;idt=289&amp;shv=r20230802&amp;mjsv=m202308010102&amp;ptt=9&amp;saldr=aa&amp;abxe=1&amp;prev_fmts=0x0&amp;nras=1&amp;correlator=3869567246326&amp;frm=20&amp;pv=1&amp;ga_vid=1384795695.1691178593&amp;ga_sid=1691179908&amp;ga_hid=94239904&amp;ga_fc=1&amp;u_tz=330&amp;u_his=11&amp;u_h=780&amp;u_w=360&amp;u_ah=780&amp;u_aw=360&amp;u_cd=24&amp;u_sd=3&amp;dmc=4&amp;adx=0&amp;ady=1481&amp;biw=360&amp;bih=620&amp;scr_x=0&amp;scr_y=0&amp;eid=44759926%2C44759842%2C44759875%2C31076482%2C31076509%2C31076653%2C31076731%2C44785295%2C31076205&amp;oid=2&amp;pvsid=1054268048300048&amp;tmod=37644952&amp;uas=0&amp;nvt=1&amp;ref=https://yo.fan&amp;fc=1920&amp;brdim=0%2C0%2C0%2C0%2C360%2C0%2C360%2C620%2C360%2C620&amp;vis=1&amp;rsz=%7C%7CeoEbr%7C&amp;abl=CS&amp;pfx=0&amp;fu=128&amp;bc=31&amp;ifi=2&amp;uci=a!2&amp;btvi=1&amp;fsb=1&amp;xpc=nz5SdMYM3E&amp;p=https://yo.fan&amp;dtd=316" data-google-container-id="a!2" data-load-complete="true"></iframe></div></ins>
 </body>
 <script>(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://inklinkor.com/tag.min.js',5833871,document.body||document.documentElement)</script>
 <script>
